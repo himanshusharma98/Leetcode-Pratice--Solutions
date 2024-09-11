440ccda951418446517f8a27488b936168fe0220
sharma <sharma@DESKTOP-IS2799H>
2024-09-11 12:42:58 +0530

    Code Formatted

--- minimum-swaps-to-group-all-1s-together-ii.js
+++ minimum-swaps-to-group-all-1s-together-ii.js
@@ -2,43 +2,42 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minSwaps = function(nums) {
+var minSwaps = function (nums) {
     const n = nums.length;
     let ones = 0;
-    for( let i = 0; i < n; i++){
-        if(nums[i] === 1) ones++;
+    for (let i = 0; i < n; i++) {
+        if (nums[i] === 1) ones++;
     }
     let minSwaps = Infinity;
     let left = 0;
     let right = 0;
     let windowOnes = 0;
 
-    while(right < n ){
-        if(nums[right] === 1) windowOnes++;
-        if(right - left + 1 > ones){
-            if(nums[left] === 1) windowOnes--;
+    while (right < n) {
+        if (nums[right] === 1) windowOnes++;
+        if (right - left + 1 > ones) {
+            if (nums[left] === 1) windowOnes--;
             left++;
         }
-        if (right - left + 1 === ones){
+        if (right - left + 1 === ones) {
             minSwaps = Math.min(minSwaps, ones - windowOnes);
         }
         right++;
     }
-    left  = n - ones;
+    left = n - ones;
     right = n - 1;
     windowOnes = 0;
 
-    while(right>=0){
-       if(nums[right] === 1) windowOnes++;
-       if(right - left + 1  > ones){
-        if(nums[left] === 1) windowOnes--;
-        left++;
-       }
-       if (right - left + 1 === ones){
-        minSwaps = Math.min(minSwaps, ones - windowOnes);
-       }
-       right--;
+    while (right >= 0) {
+        if (nums[right] === 1) windowOnes++;
+        if (right - left + 1 > ones) {
+            if (nums[left] === 1) windowOnes--;
+            left++;
+        }
+        if (right - left + 1 === ones) {
+            minSwaps = Math.min(minSwaps, ones - windowOnes);
+        }
+        right--;
     }
     return minSwaps === Infinity ? 0 : minSwaps;
 };
-       
