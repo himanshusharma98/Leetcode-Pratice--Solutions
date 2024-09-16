c19bca2c6465f5032cefd61b033a14604b3c16be
sharma <sharma@DESKTOP-IS2799H>
2024-09-16 13:08:06 +0530

    Code Formatted

--- minimum-time-difference.js
+++ minimum-time-difference.js
@@ -4,20 +4,20 @@
  */
 var findMinDifference = function (timePoints) {
     const minutes = timePoints(time => {
-        const [hour,minute] = time.split(':');
+        const [hour, minute] = time.split(':');
         return parseInt(hour) * 60 + parseInt(minute);
     });
 
-    minutes.sort((a,b) => a - b);
+    minutes.sort((a, b) => a - b);
 
     let min = Infinity;
     for (let i = 1; i < minutes.length; i++) {
         const diff = minutes[i] - minutes[i - 1];
-        minDiff = Math.min(minDiff, diff) ;
+        minDiff = Math.min(minDiff, diff);
     }
 
     const wraparoundDiff = minutes[0] + 1440 - minutes[minutes.length - 1];
-    minDiff = Math.min(minDiff,wraparoundDiff);
+    minDiff = Math.min(minDiff, wraparoundDiff);
     return minDiff;
-    
+
 }
