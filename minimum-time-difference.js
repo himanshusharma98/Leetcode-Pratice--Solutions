e3b41121f1310fb0afcd4ac1cec767ff0c50a799
sharma <sharma@DESKTOP-IS2799H>
2024-09-16 13:06:57 +0530

    LeetCode JavaScript Solution

---
+++ minimum-time-difference.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} timePoints
+ * @return {number}
+ */
+var findMinDifference = function (timePoints) {
+    const minutes = timePoints(time => {
+        const [hour,minute] = time.split(':');
+        return parseInt(hour) * 60 + parseInt(minute);
+    });
+
+    minutes.sort((a,b) => a - b);
+
+    let min = Infinity;
+    for (let i = 1; i < minutes.length; i++) {
+        const diff = minutes[i] - minutes[i - 1];
+        minDiff = Math.min(minDiff, diff) ;
+    }
+
+    const wraparoundDiff = minutes[0] + 1440 - minutes[minutes.length - 1];
+    minDiff = Math.min(minDiff,wraparoundDiff);
+    return minDiff;
+    
+}
