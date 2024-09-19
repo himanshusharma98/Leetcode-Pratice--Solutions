421ea0bc224f450b39817d9ae4049e557f3628b7
sharma <sharma@DESKTOP-IS2799H>
2024-09-19 19:04:48 +0530

    Formatted code

--- different-ways-to-add-parentheses.js
+++ different-ways-to-add-parentheses.js
@@ -2,35 +2,35 @@
  * @param {string} expression
  * @return {number[]}
  */
-var diffWaysToCompute = function(expression) {
+var diffWaysToCompute = function (expression) {
     const memo = {};
-    function compute(expr){
-        if(memo[expr]) return memo[expr];
+    function compute(expr) {
+        if (memo[expr]) return memo[expr];
         const results = [];
 
-        for(let i=0; i< expr.length; i++){
-            const char  = expr[i];
+        for (let i = 0; i < expr.length; i++) {
+            const char = expr[i];
 
-            if(char === '+' || char === '-' || char === '*'){
-                const left  = expr.slice(0,i);
+            if (char === '+' || char === '-' || char === '*') {
+                const left = expr.slice(0, i);
                 const right = expr.slice(i + 1);
 
-                const  leftResults = compute(left);
+                const leftResults = compute(left);
                 const rightResults = compute(right);
 
-                for(const leftRes of leftResults){
-                    for(const rightRes of rightResults){
+                for (const leftRes of leftResults) {
+                    for (const rightRes of rightResults) {
                         let result;
-                        switch(char){
+                        switch (char) {
                             case '+':
-                            result = leftRes + rightRes;
-                            break;
+                                result = leftRes + rightRes;
+                                break;
                             case '-':
-                            result = leftRes - rightRes;
-                            break;
+                                result = leftRes - rightRes;
+                                break;
                             case '*':
-                            result = leftRes * rightRes;
-                            break;
+                                result = leftRes * rightRes;
+                                break;
                         }
                         results.push(result);
 
@@ -38,12 +38,12 @@ var diffWaysToCompute = function(expression) {
                 }
             }
         }
-        if(results.length === 0){
+        if (results.length === 0) {
             results.push(parseInt(expr, 10));
         }
         memo[expr] = results;
         return results;
     }
     return compute(expression);
-    
+
 };
