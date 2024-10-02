42eb59b4252f547af19eb05713d37136df951d5d
sharma <sharma@DESKTOP-IS2799H>
2024-10-02 13:03:13 +0530

    Import Statements Added & Code Formatted

--- Leetcode Solutions - 30-09-2024/design-a-stack-with-increment-operation.java
+++ Leetcode Solutions - 30-09-2024/design-a-stack-with-increment-operation.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 class CustomStack {
     private int n;
     private Stack<Integer> stack;
@@ -10,22 +14,24 @@ class CustomStack {
     }
 
     public void push(int x) {
-        if(stack.size() < n){
+        if (stack.size() < n) {
             stack.push(x);
             inc.add(0);
         }
     }
 
     public int pop() {
-        if(stack.isEmpty()) return -1;
-        if (inc.size() > 1) inc.set(inc.size() - 2, inc.get(inc.size() - 2) + inc.get(inc.size() - 1));
-        return stack.pop() + inc.remove(inc.size() -1);
+        if (stack.isEmpty())
+            return -1;
+        if (inc.size() > 1)
+            inc.set(inc.size() - 2, inc.get(inc.size() - 2) + inc.get(inc.size() - 1));
+        return stack.pop() + inc.remove(inc.size() - 1);
     }
 
     public void increment(int k, int val) {
-        if(!stack.isEmpty()){
+        if (!stack.isEmpty()) {
             int index = Math.min(k, inc.size()) - 1;
-            inc.set(index,inc.get(index) + val);
+            inc.set(index, inc.get(index) + val);
         }
     }
 }
