3893b69b3cabcbf1bb4cfe232983accbd59c75c6
sharma <sharma@DESKTOP-IS2799H>
2024-10-05 13:40:50 +0530

    Code formatted

--- Leetcode Solutions - 05-10- 2024/koko-eating-bananas.ts
+++ Leetcode Solutions - 05-10- 2024/koko-eating-bananas.ts
@@ -1,33 +1,33 @@
 function minEatingSpeed(piles: number[], h: number): number {
-  
-    let left = 1;
-    let right = Math.max(...piles);
-  
-    
-    let smallestK;
-  
-    while (left <= right) {
-      const numOfBananaPerHour = left + Math.trunc((right - left) / 2);
-  
-      let totalTimeToEatAllBananas = 0;
-      for (let j = 0; j < piles.length; j++) {
-        totalTimeToEatAllBananas += Math.ceil(piles[j] / numOfBananaPerHour);
-      }
-  
-      
-      if (totalTimeToEatAllBananas <= h) {
-        
-        smallestK = numOfBananaPerHour;
-  
-        
-        right = numOfBananaPerHour - 1;
-      }
-  
-      
-      else {
-        left = numOfBananaPerHour + 1;
-      }
+
+  let left = 1;
+  let right = Math.max(...piles);
+
+
+  let smallestK;
+
+  while (left <= right) {
+    const numOfBananaPerHour = left + Math.trunc((right - left) / 2);
+
+    let totalTimeToEatAllBananas = 0;
+    for (let j = 0; j < piles.length; j++) {
+      totalTimeToEatAllBananas += Math.ceil(piles[j] / numOfBananaPerHour);
     }
-  
-    return smallestK;
+
+
+    if (totalTimeToEatAllBananas <= h) {
+
+      smallestK = numOfBananaPerHour;
+
+
+      right = numOfBananaPerHour - 1;
+    }
+
+
+    else {
+      left = numOfBananaPerHour + 1;
+    }
   }
+
+  return smallestK;
+}
--- Leetcode Solutions - 05-10- 2024/middle-of-the-linked-list.java
+++ Leetcode Solutions - 05-10- 2024/middle-of-the-linked-list.java
@@ -1,10 +1,11 @@
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode trav = head;
-        while(trav != null && trav.next != null){
+        while (trav != null && trav.next != null) {
             trav = trav.next.next;
             head = head.next;
-        } return head;
-        
+        }
+        return head;
+
     }
 }
