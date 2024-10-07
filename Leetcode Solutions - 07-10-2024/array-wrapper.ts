c2ca2a71f045481c3c8e303a36516b19a468c377
sharma <sharma@DESKTOP-IS2799H>
2024-10-07 21:43:27 +0530

    Code Formatted

--- Leetcode Solutions - 07-10-2024/array-wrapper.ts
+++ Leetcode Solutions - 07-10-2024/array-wrapper.ts
@@ -1,21 +1,21 @@
 class ArrayWrapper {
     private nums: number[];
-    
+
     constructor(nums: number[]) {
         this.nums = nums;
-        
+
     }
-    
+
     valueOf(): number {
         return this.nums.reduce((acc, curr) => acc + curr, 0);
-        
+
     }
-    
+
     toString(): string {
         return JSON.stringify(this.nums);
-        
+
     }
 };
-const obj1 = new ArrayWrapper([1,2]);
+const obj1 = new ArrayWrapper([1, 2]);
 const obj2 = new ArrayWrapper([3, 4]);
 
