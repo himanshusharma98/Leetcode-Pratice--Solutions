178ce335f80bc1394eb9811e0e8b5d0f42e47ad7
sharma <sharma@DESKTOP-IS2799H>
2024-10-17 12:57:48 +0530

    Leetcode Solutions - 17-10-2024

---
+++ Leetcode Solutions - 17-10-2024/maximum-swap.ts
@@ -0,0 +1,31 @@
+function maximumSwap(num: number): number {
+    let str: string = num.toString();
+    let num2: string[] = str.split('');
+    let n = num2.length;
+
+    let index: number[] = new Array(n);
+    let maxi: number = parseInt(num2[n-1]),idx = n-1;
+    index[n-1] = idx;
+
+    for(let i = n-2;i >= 0;i--){
+        if(parseInt(num2[i]) > maxi) {
+            maxi = parseInt(num2[i]);
+            idx = i;
+        }
+        index[i] = idx;
+    }
+
+    let i = 0;
+    while(i < n){
+        if(parseInt(num2[index[i]]) > parseInt(num2[i])){
+            let temp = num2[index[i]];
+            num2[index[i]] = num2[i];
+            num2[i] = temp;
+            break;
+        }
+        i++;
+    }
+
+    let ans: number = parseInt(num2.join(''));
+    return ans;
+};
---
+++ Leetcode Solutions - 17-10-2024/trim-a-binary-search-tree.java
@@ -0,0 +1,30 @@
+// Uncomment the TreeNode class definition
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    
+    public TreeNode trimBST(TreeNode node, int low, int high) {
+        if(node == null)return null;
+
+  
+        node.left = trimBST(node.left, low, high);
+        node.right = trimBST(node.right, low, high);
+
+        if(node.val > high || node.val < low){
+            if(node.left != null)return node.left;
+            else return node.right;
+        }
+        return node;
+    }
+}
