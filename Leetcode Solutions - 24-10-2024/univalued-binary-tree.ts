d5d31280cd5fa7825b816d2f7aad20e024c1ba75
Himanshu Sharma <97788582+himanshusharma98@users.noreply.github.com>
2024-10-25 11:20:01 +0530

    Create univalued-binary-tree.ts

---
+++ Leetcode Solutions - 24-10-2024/univalued-binary-tree.ts
@@ -0,0 +1,12 @@
+function isUnivalTree(root: TreeNode | null): boolean {
+    const vals = new Array<number>();
+    const dfs = (node: TreeNode | null): void => {
+        if(node){
+            vals.push(node.val);
+            dfs(node.left);
+            dfs(node.right);
+        }
+    }
+    dfs(root);
+    return vals.reduce((acc, curr) => acc + curr) /  vals.length === vals[0]
+};
