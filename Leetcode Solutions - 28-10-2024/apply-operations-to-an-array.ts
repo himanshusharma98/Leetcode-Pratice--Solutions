6835b46463fcd5d772117e973197042765311b2d
Himanshu Sharma <97788582+himanshusharma98@users.noreply.github.com>
2024-10-28 13:27:42 +0530

    Create apply-operations-to-an-array.ts

---
+++ Leetcode Solutions - 28-10-2024/apply-operations-to-an-array.ts
@@ -0,0 +1,12 @@
+function applyOperations(nums: number[]): number[] {
+
+  for (let i = 0; i < nums.length-1; i++) {
+      if (nums[i] == nums[i+1]) {
+        nums[i] *= 2
+        nums[i+1] = 0
+      }
+  }
+
+  let filterZero = nums.filter(e => e !== 0)
+  return filterZero.concat(new Array(nums.length - filterZero.length).fill(0))
+};
