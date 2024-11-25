2a7a93f2a51a56d474bb38e49aa3c48bf120c6a2
Himanshu Sharma <97788582+himanshusharma98@users.noreply.github.com>
2024-11-25 18:14:31 +0530

    Create timeout-cancellation.js

---
+++ Leetcode Solution - 08-11-2024/timeout-cancellation.js
@@ -0,0 +1,12 @@
+const cancellable = function(fn, args, t) {
+    // cancelFn function//
+    const cancelFn = function (){
+      clearTimeout(timer);
+  };
+  const timer = setTimeout(()=>{
+      fn(...args)
+  }, t);
+  return cancelFn ;
+};
+
+
