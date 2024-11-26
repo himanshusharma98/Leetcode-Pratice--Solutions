6ac9cf2e131812853de3460b51d80a2bfa51837e
Himanshu Sharma <97788582+himanshusharma98@users.noreply.github.com>
2024-11-26 11:43:41 +0530

    Create filter-elements-from-array.js

---
+++ Leetcode Solutions - 22-11-2024/filter-elements-from-array.js
@@ -0,0 +1,9 @@
+var filter = function(arr, fn) {
+    let fil = []
+    for(let i = 0; i < arr.length; i++){
+        if(fn(arr[i], i)) {
+            fil.push(arr[i]);
+        }
+    }
+    return fil
+};
