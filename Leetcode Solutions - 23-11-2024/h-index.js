26ec7cba14e700521f530d767dc5d3eaedd91cf8
Himanshu Sharma <97788582+himanshusharma98@users.noreply.github.com>
2024-11-26 11:56:52 +0530

    Create h-index.js

---
+++ Leetcode Solutions - 23-11-2024/h-index.js
@@ -0,0 +1,16 @@
+var hIndex = function(citations) {
+    let papers = citations.length;
+    let citationBuckets = new Array(papers + 1).fill(0);
+
+    for (let citation of citations) {
+        citationBuckets[Math.min(citation, papers)] += 1;
+    }
+
+    let cumulativePapers = 0;
+    for (let hIndex = papers; hIndex >= 0; hIndex--) {
+        cumulativePapers += citationBuckets[hIndex];
+        if (cumulativePapers >= hIndex) {
+            return hIndex;
+        }
+    }    
+};
